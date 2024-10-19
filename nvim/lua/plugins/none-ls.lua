ab62b59a50c8c45189ae009407407af8ce3881bb
Liken Tan <15458805+likendev@users.noreply.github.com>
2024-10-19 16:07:22 +0800

    null-ls: fix eslint_d diagnostics

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,6 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = { "nvimtools/none-ls-extras.nvim" },
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
@@ -9,10 +10,10 @@ return {
                         return utils.root_has_file({ "stylua.toml", ".stylua.toml" })
                     end,
                 }),
-                null_ls.builtins.diagnostics.eslint_d,
                 null_ls.builtins.formatting.prettierd.with {
                     filetypes = { "html", "json", "svelte", "markdown", "css", "javascript", "javascriptreact" }
                 },
+                require("none-ls.diagnostics.eslint_d"),
             },
         })
         vim.keymap.set("n", "<space>f", function()
