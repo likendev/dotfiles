b996c82e23051336f763bdd07aa7d04f339145f2
likendev <15458805+likendev@users.noreply.github.com>
2026-05-27 01:44:09 +0800

    fix(nvim): fix leader key bug

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Install Plugin Manager (lazy.nvim)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
